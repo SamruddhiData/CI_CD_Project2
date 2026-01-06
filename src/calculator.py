e667626988dd471826ba635ab338d155e33619ce
Sam95 <samruddhisandipgunjal@gmail.com>
2026-01-06 20:31:01 +0530

    Initial commit for calculator CI/CD project

---
+++ src/calculator.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    if b==0:
+        raise ValueError('can not divide by zero')
+    return a / b
+if __name__ == "__ main__ ":
+    print("Add:",add(10, 5))
+    print("Subtract:",subtract(10, 5))
+    print("Multiply:",multiply(10, 5))
+    print("Divide:",divide(10, 5))
---
+++ tests/test_calculator.py
@@ -0,0 +1,21 @@
+import pytest
+from src.calculator import add, substrat,multiply,divide
+
+def test_add():
+    assert add(2, 3) == 5
+
+
+def test_subtract():
+    assert substrat(5, 3) == 2
+
+def test_multiply():
+    assert multiply(4, 3) == 12
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(10, 0)
+        
+            
